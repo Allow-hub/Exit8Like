3ce499a00c44c909fd6c98e5e79386fddeb7aa89
22RENT-16\student <y24340216@gmail.com>
2024-07-06 13:12:25 +0900

    タイトル仮置き

---
+++ Assets/Scripts/Title/TitleButtonInput.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleButtonInput : MonoBehaviour
+{
+    [SerializeField] Button startButton,optionButton,exitButton;
+    [SerializeField] GameObject mainCanvas, optionCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCanvas.SetActive(true);
+        optionCanvas.SetActive(false);
+        startButton.onClick.AddListener(OnInGame);
+        optionButton.onClick.AddListener(OnOption);
+        exitButton.onClick.AddListener(OnExit);
+    }
+
+    public  void OnInGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnOption()
+    {
+        optionCanvas.gameObject.SetActive(true);
+    }
+
+    public void OnExit()
+    {
+        optionCanvas.gameObject.SetActive(false);
+        mainCanvas.SetActive(true);
+    }
+}
