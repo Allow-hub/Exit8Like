f4a31faf1e556ef172843c94d807d37184cd0533
DESKTOP-AB8J0V3\PC_User <y24340216@gmail.com>
2024-07-09 19:18:04 +0900

    PostProcessing

--- Assets/Scripts/InGame/Area/TurnBackCollider.cs
+++ Assets/Scripts/InGame/Area/TurnBackCollider.cs
@@ -1,11 +1,18 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TurnBackCollider : MonoBehaviour
 {
     public delegate void TurnBack();
     public static TurnBack onTurnBack;
+    [SerializeField] private Image fade;
+    [SerializeField] private GameObject player;
+    Rigidbody rb;
+    private void Start()
+    {
+        rb=player.GetComponent<Rigidbody>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -14,6 +21,49 @@ public class TurnBackCollider : MonoBehaviour
         {
             GameManager.Instance.isBack = true;
             onTurnBack?.Invoke();
+            StartCoroutine(FadeIn());
         }
+    }
+
+    private IEnumerator FadeIn()
+    {
+        rb.velocity = Vector3.zero;
+        PlayerController playerController = player.GetComponent<PlayerController>();
+        playerController.enabled = false;
+        RectTransform rectTransform = fade.GetComponent<RectTransform>();
+        Vector2 startSize = new Vector2(0, 0);
+        Vector2 endSize = new Vector2(1920, 1920);
+        float duration = 1.0f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            rectTransform.sizeDelta = Vector2.Lerp(startSize, endSize, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        rectTransform.sizeDelta = endSize;
+        yield return new  WaitForSeconds(1f);
+        StartCoroutine(FadeOut());
+    }
+    private IEnumerator FadeOut()
+    {
+        RectTransform rectTransform = fade.GetComponent<RectTransform>();
+        Vector2 startSize = new Vector2(1920, 1920);
+        Vector2 endSize = new Vector2(0, 0);
+        float duration = 1.0f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            rectTransform.sizeDelta = Vector2.Lerp(startSize, endSize, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        PlayerController playerController = player.GetComponent<PlayerController>();
+        playerController.enabled = true;
+        rectTransform.sizeDelta = endSize;
     }
 }
