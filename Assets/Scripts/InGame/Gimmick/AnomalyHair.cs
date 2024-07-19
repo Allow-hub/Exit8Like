6b0f957b256f4823e213457972483b41f0dd1fad
22RENT-16\student <y24340216@gmail.com>
2024-07-19 17:21:21 +0900

    髪追いかけ

---
+++ Assets/Scripts/InGame/Gimmick/AnomalyHair.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnomalyHair : AnomalyBase
+{
+    [SerializeField] private bool isClear = false;
+    [SerializeField] private float distanceFromPlayer = 2f;
+    [SerializeField] private Sprite[] normalHair;
+    [SerializeField] private GameObject moveHair;
+    [SerializeField] private float coolDown = 0.3f;
+    [SerializeField] private float moveSpeed=1;
+    [SerializeField] private Rigidbody rb;
+
+    private bool reverseOrder = false;
+    private void Start()
+    {
+        SetProperety();
+        StartCoroutine(ChangeSprite());
+        Move();
+
+    }
+
+    private void Update()
+    {
+    }
+
+    private void SetProperety()
+    {
+        IsClear = isClear;
+        DistanceFromPlayer = distanceFromPlayer;
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector3(moveSpeed, 0, 0);
+    }
+
+    private IEnumerator ChangeSprite()
+    {
+        while (true)
+        {
+            if (!reverseOrder)
+            {
+                for (int i = 0; i < normalHair.Length; i++)
+                {
+                    spriteRenderer.sprite = normalHair[i];
+                    yield return new WaitForSeconds(coolDown);
+                }
+            }
+            else
+            {
+                for (int i = normalHair.Length - 1; i >= 0; i--)
+                {
+                    spriteRenderer.sprite = normalHair[i];
+                    yield return new WaitForSeconds(coolDown);
+                }
+            }
+
+            reverseOrder = !reverseOrder;
+        }
+    }
+}
