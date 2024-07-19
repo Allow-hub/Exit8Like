199ef803dc492f3ec3dd9449bfa0d975842cbce6
Millan50 <1015aika256431@gmail.com>
2024-07-19 19:31:22 +0900

    poster3の異変

--- Assets/Scripts/InGame/Gimmick/AnomalyPoster3.cs
+++ Assets/Scripts/InGame/Gimmick/AnomalyPoster3.cs
@@ -11,20 +11,19 @@ public class AnomalyPoster3 : AnomalyBase
     [SerializeField] private GameObject anomalyPoster;
     [SerializeField] private GameObject blackEye;
     [SerializeField] private GameObject whiteEye;
-    [SerializeField] private GameObject target;
     [SerializeField] private GameObject RightEyeParts1;
     [SerializeField] private GameObject RightEyeParts2;
     [SerializeField] private GameObject LeftEyeParts1;
     [SerializeField] private GameObject LeftEyeParts2;
+    [SerializeField] private GameObject player;
 
     Vector3 RightEyePos1;
     Vector3 RightEyePos2;
     Vector3 LeftEyePos1;
     Vector3 LeftEyePos2;
 
-    public Transform player;
-    public float moveSpeed = 1.0f;
 
+    public float moveSpeed = 1.0f; // 黒目の動くスピード
 
     // Start is called before the first frame update
     void Start()
@@ -35,23 +34,34 @@ public class AnomalyPoster3 : AnomalyBase
         blackEye.SetActive(true);
         whiteEye.SetActive(true);
 
+        // 範囲指定のObjectのTrnsform
         RightEyePos1 = RightEyeParts1.transform.position;
         RightEyePos2 = RightEyeParts2.transform.position;
         LeftEyePos1 = LeftEyeParts1.transform.position;
         LeftEyePos2 = LeftEyeParts2.transform.position;
 
- 
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+
+        //黒目の位置を範囲内に制限
+        blackEye.transform.position = new Vector3(
+            Mathf.Clamp (blackEye.transform.position.x, RightEyePos1.x, RightEyePos2.x),
+            Mathf.Clamp (blackEye.transform.position.x,LeftEyePos1.x, LeftEyePos2.x),
+            blackEye.transform.position.y
+            );
+
     }
 
     private void Move()
     {
-        
-    
+        if(GameManager.Instance==null) return;
+        float distance = Vector3.Distance(anomalyPoster.gameObject.transform.position, GameManager.Instance.player.transform.position);
+        Debug.Log (distance);
+
+
     }
 }
