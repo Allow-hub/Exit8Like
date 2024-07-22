be0da9c76ff20b4ad4502f9315705948c5f45ed2
Millan50 <1015aika256431@gmail.com>
2024-07-22 13:40:07 +0900

    アニメーションの変更

--- Assets/Scripts/InGame/Player/PlayerAnimation.cs
+++ Assets/Scripts/InGame/Player/PlayerAnimation.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
@@ -31,14 +32,20 @@ public class PlayerAnimation : MonoBehaviour
         while (true)
         {
             if (!reverseOrder)
-            for (int i = 0; i < PlayerLeft.Length; i++)
             {
-                spriteRenderer.sprite = PlayerLeft[i];
-                yield return new WaitForSeconds(Cooldown);
+                for (int i = 0; i < PlayerLeft.Length; i++)
+                {
+                    spriteRenderer.sprite = PlayerLeft[i];
+                    yield return new WaitForSeconds(Cooldown);
+                }
             }
             else
             {
-                for (int i = 0;i < PlayerRight.Length -1; i--) { }
+                for (int i = PlayerLeft.Length - 1; i >= 0; i--)
+                {
+                    spriteRenderer.sprite = PlayerLeft[i];
+                    yield return new WaitForSeconds(Cooldown);
+                }
             }
         }
     }
@@ -47,10 +54,22 @@ public class PlayerAnimation : MonoBehaviour
     {
         while (true)
         {
-            for (int i = 0; i < PlayerRight.Length; i++)
+            if (!reverseOrder)
             {
-                spriteRenderer.sprite = PlayerRight[i];
-                yield return new WaitForSeconds(Cooldown);
+                for (int i = 0; i < PlayerRight.Length; i++)
+                {
+                    spriteRenderer.sprite = PlayerRight[i];
+                    yield return new WaitForSeconds(Cooldown);
+                }
+            }
+            else
+            {
+                for (int i = PlayerRight.Length; i >= 0; i--)
+                {
+                    spriteRenderer.sprite = PlayerRight[i];
+                    yield return new WaitForSeconds(Cooldown);
+                }
+
             }
         }
     }
