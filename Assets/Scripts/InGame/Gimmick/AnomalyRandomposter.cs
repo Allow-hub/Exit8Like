1952482c7da00b918ab12f993891184dc8c3c973
Millan50 <1015aika256431@gmail.com>
2024-07-26 17:50:28 +0900

    画像の変更と異変の追加

--- Assets/Scripts/InGame/Gimmick/AnomalyRandomposter.cs
+++ Assets/Scripts/InGame/Gimmick/AnomalyRandomposter.cs
@@ -2,8 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnomalyRandomposter : MonoBehaviour
+public class AnomalyRandomposter : AnomalyBase
 {
+    [SerializeField] private bool isClear = false;
+    [SerializeField] private float distanceFromPlayer = 200f;
+    [SerializeField] private int number;
+    [SerializeField] private string explain;
+
+    [SerializeField] private GameObject poster1;
+    [SerializeField] private GameObject poster2;
+    [SerializeField] private GameObject poster3;
+    [SerializeField] private GameObject poster4;
+    [SerializeField] private GameObject poster5;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
