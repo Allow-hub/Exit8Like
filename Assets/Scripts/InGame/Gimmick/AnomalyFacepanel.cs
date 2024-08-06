816e9588fc10ff37c45d7e3f754e9a04c205bd47
Millan50 <1015aika256431@gmail.com>
2024-08-06 17:33:53 +0900

    異変の細かい修正

---
+++ Assets/Scripts/InGame/Gimmick/AnomalyFacepanel.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnomalyFacepanel : AnomalyBase
+{
+    [SerializeField] private bool isClear = false;
+    [SerializeField] private float distanceFromPlayer = 200f;
+    [SerializeField] private int number;
+    [SerializeField] private string explain;
+
+    [SerializeField] private GameObject Kohame;
+    [SerializeField] private GameObject KohameAnomaly;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetProperety();
+        Kohame.SetActive(true);
+        KohameAnomaly.SetActive(false);
+
+    }
+    private void SetProperety()
+    {
+        IsClear = isClear;
+        DistanceFromPlayer = distanceFromPlayer;
+        Number = number;
+        Explain = explain;
+    }
+
+    public override void Animation()
+    {
+        Kohame.SetActive(false);
+        KohameAnomaly.SetActive(true);
+    }
+
+    public override void ReverseAnomaly()
+    {
+        base.ReverseAnomaly();
+        Kohame.SetActive(true);
+        KohameAnomaly.SetActive(false);
+
+    }
+
+}
